98b68bd3ca2649b08c3e5e42130fbc347745e1ad
Alec Nunez <anunez@unca.edu>
2016-10-13 14:22:13 -0400

    Initial Commit to setup project. Imported drawings.jar to project and associated javadoc. Created main class called Draw. Connected project with Ecplise to github.

---
+++ src/Draw.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author aclink4126
+ *
+ */
+public class Draw {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
