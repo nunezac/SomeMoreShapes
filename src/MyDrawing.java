6e36f68c4258ffd31b97a42374257f8eae191a8f
Alec Nunez <anunez@unca.edu>
2016-10-14 21:06:49 -0400

    Simplified and cleaned up quite a bit of code by creating one general method for drawing all of the icons on the screen and placing each of the "design" maps for the icons with the enum Icon. This allows for me to pass only the enum type to the method drawIcon and then the Icon holds all of the information needed to run. Additionally fixed a small logical error within Icon.leftBound() that did not return the correct bound for some of the icons.

--- src/MyDrawing.java
+++ src/MyDrawing.java
@@ -1,6 +1,4 @@
-//import java.awt.event.ComponentEvent;
 import java.awt.event.MouseEvent;
-//import java.awt.Color;
 
 /**
  * 
@@ -13,6 +11,7 @@ import java.awt.event.MouseEvent;
 public class MyDrawing extends csci348.drawings.SimpleDrawing {
 	
 	private final static int ICONSIZE = 40;
+	
 	private static Icon selectedIcon = Icon.POINTER;
 	private boolean isDrawing = false;
 	private int[] savedPoint = {0, 0};
@@ -20,6 +19,117 @@ public class MyDrawing extends csci348.drawings.SimpleDrawing {
 	private enum Icon {
 		POINTER, CIRCLE, BOX, PARALLELAGRAM,
 		DIAMOND, TRIANGLE, ARROW, LINE, TRASH;
+		
+		private final int[][] POINTERDESIGN = {
+				{0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0, 0, 1, 2, 3,
+				4, 5, 5, 6, 6, 7, 8, 9, 9, 8,
+				8, 7, 7, 8, 9, 10, 11, 11, 10,
+				9, 8, 7, 6, 5, 4, 3, 2, 1},
+				{0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+				10, 11, 12, 13, 14, 15, 16, 16, 15, 14,
+				13, 14, 15, 16, 17, 18, 18, 17, 16, 15,
+				14, 13, 12, 12, 12, 12, 12, 11, 10, 9,
+				8, 7, 6, 5, 4, 3, 2, 1}};
+		private final int[][] CIRCLEDESIGN = {
+				{0, 0, 0, 0, 1, 1, 1, 2, 2, 3,
+				4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+				14, 15, 16, 17, 18, 19, 19, 20, 20, 20,
+				21, 21, 21, 21, 20, 20, 20, 19, 19, 18,
+				17, 16, 15, 14, 13, 12, 11, 10, 9, 8,
+				7, 6, 5, 4, 3, 2, 2, 1, 1, 1},
+				{9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+				19, 19, 20, 20, 20, 21, 21, 21, 21, 20,
+				20, 20, 19, 19, 18, 17, 16, 15, 14, 13,
+				12, 11, 10, 9, 8, 7, 6, 5, 4, 3,
+				2, 2, 1, 1, 1, 0, 0, 0, 0, 1,
+				1, 1, 2, 2, 3, 4, 5, 6, 7, 8}};
+		private final int[][] BOXDESIGN = {
+				{0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 1, 2, 3, 4, 5, 6, 7, 8,
+				9, 10, 11, 12, 13, 14, 15, 16, 17, 17,
+				17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
+				16, 15, 14, 13, 12, 11, 10, 9, 8, 7,
+				6, 5, 4, 3, 2, 1},
+				{0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+				10, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+				11, 11, 11, 11, 11, 11, 11, 11, 11, 10,
+				9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
+				0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+				0, 0, 0, 0, 0, 0}
+		};
+		private final int[][] PARALLELAGRAMDESIGN = {
+				{6, 6, 5, 5, 4, 4, 3, 3, 2, 2,
+					1, 1, 0, 0, 1, 2, 3, 4, 5, 6,
+					7, 8, 9, 10, 11, 12, 13, 14, 15, 15,
+					16, 16, 17, 17, 18, 18, 19, 19, 20, 20,
+					21, 21, 20, 19, 18, 17, 16, 15, 14, 13,
+					12, 11, 10, 9, 8, 7},
+				{1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+						11, 12, 13, 14, 15, 15, 15, 15, 15, 15,
+						15, 15, 15, 15, 15, 15, 15, 15, 14, 13,
+						12, 11, 10, 9, 8, 7, 6, 5, 4, 3,
+						2, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 0}
+		};
+		private final int[][] DIAMONDDESIGN = {
+				{0, 1, 1, 2, 3, 3, 4, 5, 5, 6,
+					7, 7, 8, 9, 9, 10, 11, 11, 12, 11,
+					11, 10, 9, 9, 8, 7, 7, 6, 5, 5,
+					4, 3, 3, 2, 1, 1},
+				{9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+						17, 16, 15, 14, 13, 12, 11, 10, 9, 8,
+						7, 6, 5, 4, 3, 2, 1, 0, 1, 2,
+						3, 4, 5, 6, 7, 8}
+		};
+		private final int[][] TRIANGLEDESIGN = {
+				{0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+					10, 11, 12, 13, 14, 14, 13, 12, 12, 11,
+					10, 10, 9, 8, 8, 7, 6, 6, 5, 4,
+					4, 3, 2, 2, 1, 0},
+				{11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+						11, 11, 11, 11, 11, 10, 9, 8, 7, 6,
+						5, 4, 3, 2, 1, 0, 1, 2, 3, 4,
+						5, 6, 7, 8, 9, 10}
+		};
+		private final int[][] ARROWDESIGN = {
+				{0, 1, 1, 2, 2, 2, 3, 3, 3, 3,
+					4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+					4, 4, 4, 4, 5, 5, 5, 5, 6, 6,
+					6, 7, 7, 8},
+				{4, 3, 4, 2, 3, 4, 1, 2, 3, 4,
+						0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+						10, 11, 12, 13, 1, 2, 3, 4, 2, 3,
+						4, 3, 4, 4}
+		};
+		private final int[][] LINEDESIGN = {
+				{0, 0, 1, 1, 1, 2, 2, 2, 3, 3,
+					3, 4, 4, 4, 5, 5, 5, 6, 6, 6,
+					7, 7, 7, 8, 8},
+				{16, 15, 15, 14, 13, 13, 12, 11, 11, 10,
+						9, 9, 8, 7, 7, 6, 5, 5, 4, 3,
+						3, 2, 1, 1, 0}
+		};
+		private final int[][] TRASHDESIGN = {
+				{0, 0, 0, 1, 1, 1, 1, 1, 1, 1,
+					1, 1, 1, 1, 1, 1, 2, 3, 4, 5,
+					6, 7, 8, 9, 9, 9, 9, 9, 9, 9,
+					9, 9, 9, 9, 9, 9, 8, 7, 6, 5,
+					4, 3, 2, 3, 3, 3, 3, 3, 5, 5,
+					5, 5, 7, 7, 7, 7, 7, 4, 3, 3,
+					3, 4, 5, 6, 7, 7, 7, 6, 10, 10,
+					10, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+					3, 4, 5, 6, 7},
+				{3, 4, 5, 5, 6, 7, 8, 9, 10, 11,
+						12, 13, 14, 15, 16, 17, 17, 17, 17, 17,
+						17, 17, 17, 17, 16, 15, 14, 13, 12, 11,
+						10, 9, 8, 7, 6, 5, 5, 5, 5, 5,
+						5, 5, 5, 11, 12, 13, 14, 15, 12, 13,
+						14, 15, 11, 12, 13, 14, 15, 7, 7, 8,
+						9, 9, 9, 9, 9, 8, 7, 7, 5, 4,
+						3, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+						1, 0, 0, 0, 1}
+		};
 		
 		public static void setHighlight(int width) {
 			if (width < POINTER.rightBound()) {
@@ -58,11 +168,11 @@ public class MyDrawing extends csci348.drawings.SimpleDrawing {
 			case TRIANGLE:
 				return ICONSIZE * 5;
 			case ARROW:
-				return ICONSIZE * 5;
-			case LINE:
 				return ICONSIZE * 6;
-			case TRASH:
+			case LINE:
 				return ICONSIZE * 7;
+			case TRASH:
+				return ICONSIZE * 8;
 			default:
 				return -1;
 			}
@@ -92,6 +202,31 @@ public class MyDrawing extends csci348.drawings.SimpleDrawing {
 				return -1;
 			}
 		}
+		
+		public int[][] design() {
+			switch(this) {
+			case POINTER:
+				return POINTERDESIGN;
+			case CIRCLE:
+				return CIRCLEDESIGN;
+			case BOX:
+				return BOXDESIGN;
+			case PARALLELAGRAM:
+				return PARALLELAGRAMDESIGN;
+			case DIAMOND:
+				return DIAMONDDESIGN;
+			case TRIANGLE:
+				return TRIANGLEDESIGN;
+			case ARROW:
+				return ARROWDESIGN;
+			case LINE:
+				return LINEDESIGN;
+			case TRASH:
+				return TRASHDESIGN;
+			default:
+				return POINTERDESIGN;
+			}
+		}
 	}
 	
 	public MyDrawing() {
@@ -99,10 +234,6 @@ public class MyDrawing extends csci348.drawings.SimpleDrawing {
 		
 		redrawPage();
 	}
-	
-//	MyDrawing(int width, int height) {
-//		super(width, height);
-//	}
 	
 	@Override
 	public void mouseClicked(MouseEvent event) {
@@ -144,302 +275,61 @@ public class MyDrawing extends csci348.drawings.SimpleDrawing {
 			}
 		}
 		
-		drawPointerIcon(0, 0);
-		drawCircleIcon(ICONSIZE, 0);
-		drawBoxIcon(ICONSIZE * 2, 0);
-		drawParallelIcon(ICONSIZE * 3, 0);
-		drawDiamondIcon(ICONSIZE * 4, 0);
-		drawTriangleIcon(ICONSIZE * 5, 0);
-		drawArrowIcon(ICONSIZE * 6, 0);
-		drawLineIcon(ICONSIZE * 7, 0);
-		drawTrashIcon(ICONSIZE * 8, 0);
+		drawIcon(Icon.POINTER);
+		drawIcon(Icon.CIRCLE);
+		drawIcon(Icon.BOX);
+		drawIcon(Icon.PARALLELAGRAM);
+		drawIcon(Icon.DIAMOND);
+		drawIcon(Icon.TRIANGLE);
+		drawIcon(Icon.ARROW);
+		drawIcon(Icon.LINE);
+		drawIcon(Icon.TRASH);
+		
+		if (isDrawing) {
+			drawPoint(savedPoint[0], savedPoint[1]);
+		}
 		
 		Line.drawAllLines(this);
 	}
 	
-	private void drawPointerIcon(int startX, int startY) {
-		
-		int displaceX = ((ICONSIZE - 11) / 2) + startX;
-		int displaceY = ((ICONSIZE - 18) / 2) + startY;
-		
-		int[] designX = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 0, 0, 1, 2, 3,
-				4, 5, 5, 6, 6, 7, 8, 9, 9, 8,
-				8, 7, 7, 8, 9, 10, 11, 11, 10,
-				9, 8, 7, 6, 5, 4, 3, 2, 1};
-		int[] designY = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-				10, 11, 12, 13, 14, 15, 16, 16, 15, 14,
-				13, 14, 15, 16, 17, 18, 18, 17, 16, 15,
-				14, 13, 12, 12, 12, 12, 12, 11, 10, 9,
-				8, 7, 6, 5, 4, 3, 2, 1};
-		
-		if (selectedIcon == Icon.POINTER) {
-			toggleHighlighting(startX, startY, true);
-			
-			for (int i = 0; i < designX.length; i++) {
-				hidePoint(displaceX + designX[i], displaceY + designY[i]);
-			}
-		} else {
-			toggleHighlighting(startX, startY, false);
-			
-			for (int i = 0; i < designX.length; i++) {
-				showPoint(displaceX + designX[i], displaceY + designY[i]);
-			}
+	private void drawPoint(int x, int y) {
+		int translateX = x - 1;
+		int translateY = y - 1;
+		for (int i = 0; i < 9; i++) {
+			showPoint(translateX + (i % 3), translateY + (i / 3));
 		}
-		
-		
 	}
 	
-	private void drawCircleIcon(int startX, int startY) {
+	private void drawIcon(Icon icon) {
+		int[][] design = icon.design();
+		int displaceX = ((ICONSIZE - maxInt(design[0])) / 2) + icon.leftBound();
+		int displaceY = ((ICONSIZE - maxInt(design[1])) / 2);
 		
-		int displaceX = ((ICONSIZE - 21) / 2) + startX;
-		int displaceY = ((ICONSIZE - 21) / 2) + startY;
-		
-		int[] designX = {0, 0, 0, 0, 1, 1, 1, 2, 2, 3,
-				4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-				14, 15, 16, 17, 18, 19, 19, 20, 20, 20,
-				21, 21, 21, 21, 20, 20, 20, 19, 19, 18,
-				17, 16, 15, 14, 13, 12, 11, 10, 9, 8,
-				7, 6, 5, 4, 3, 2, 2, 1, 1, 1};
-		int[] designY = {9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
-				19, 19, 20, 20, 20, 21, 21, 21, 21, 20,
-				20, 20, 19, 19, 18, 17, 16, 15, 14, 13,
-				12, 11, 10, 9, 8, 7, 6, 5, 4, 3,
-				2, 2, 1, 1, 1, 0, 0, 0, 0, 1,
-				1, 1, 2, 2, 3, 4, 5, 6, 7, 8};
-		
-		if (selectedIcon == Icon.CIRCLE) {
-			toggleHighlighting(startX, startY, true);
+		if (selectedIcon == icon) {
+			toggleHighlighting(icon.leftBound(), 0, true);
 			
-			for (int i = 0; i < designX.length; i++) {
-				hidePoint(displaceX + designX[i], displaceY + designY[i]);
+			for (int i = 0; i < design[0].length; i++) {
+				hidePoint(displaceX + design[0][i], displaceY + design[1][i]);
 			}
 		} else {
-			toggleHighlighting(startX, startY, false);
+			toggleHighlighting(icon.leftBound(), 0, false);
 			
-			for (int i = 0; i < designX.length; i++) {
-				showPoint(displaceX + designX[i], displaceY + designY[i]);
+			for (int i = 0; i < design[0].length; i++) {
+				showPoint(displaceX + design[0][i], displaceY + design[1][i]);
 			}
 		}
 	}
 	
-	private void drawBoxIcon(int startX, int startY) {
+	private int maxInt(int[] intArray) {
+		int max = 0;
 		
-		int displaceX = ((ICONSIZE - 17) / 2) + startX;
-		int displaceY = ((ICONSIZE - 11) / 2) + startY;
-		
-		int[] designX = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-				0, 0, 1, 2, 3, 4, 5, 6, 7, 8,
-				9, 10, 11, 12, 13, 14, 15, 16, 17, 17,
-				17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
-				16, 15, 14, 13, 12, 11, 10, 9, 8, 7,
-				6, 5, 4, 3, 2, 1};
-		int[] designY = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-				10, 11, 11, 11, 11, 11, 11, 11, 11, 11,
-				11, 11, 11, 11, 11, 11, 11, 11, 11, 10,
-				9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
-				0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 0};
-		
-		if (selectedIcon == Icon.BOX) {
-			toggleHighlighting(startX, startY, true);
-			
-			for (int i = 0; i < designX.length; i++) {
-				hidePoint(displaceX + designX[i], displaceY + designY[i]);
-			}
-		} else {
-			toggleHighlighting(startX, startY, false);
-			
-			for (int i = 0; i < designX.length; i++) {
-				showPoint(displaceX + designX[i], displaceY + designY[i]);
+		for (int i = 0; i < intArray.length; i++) {
+			if (intArray[i] > max) {
+				max = intArray[i];
 			}
 		}
-	}
-	
-	private void drawParallelIcon(int startX, int startY) {
 		
-		int displaceX = ((ICONSIZE - 21) / 2) + startX;
-		int displaceY = ((ICONSIZE - 15) / 2) + startY;
-		
-		int[] designX = {6, 6, 5, 5, 4, 4, 3, 3, 2, 2,
-				1, 1, 0, 0, 1, 2, 3, 4, 5, 6,
-				7, 8, 9, 10, 11, 12, 13, 14, 15, 15,
-				16, 16, 17, 17, 18, 18, 19, 19, 20, 20,
-				21, 21, 20, 19, 18, 17, 16, 15, 14, 13,
-				12, 11, 10, 9, 8, 7};
-		int[] designY = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-				11, 12, 13, 14, 15, 15, 15, 15, 15, 15,
-				15, 15, 15, 15, 15, 15, 15, 15, 14, 13,
-				12, 11, 10, 9, 8, 7, 6, 5, 4, 3,
-				2, 1, 0, 0, 0, 0, 0, 0, 0, 0,
-				0, 0, 0, 0, 0, 0};
-		
-		if (selectedIcon == Icon.PARALLELAGRAM) {
-			toggleHighlighting(startX, startY, true);
-			
-			for (int i = 0; i < designX.length; i++) {
-				hidePoint(displaceX + designX[i], displaceY + designY[i]);
-			}
-		} else {
-			toggleHighlighting(startX, startY, false);
-			
-			for (int i = 0; i < designX.length; i++) {
-				showPoint(displaceX + designX[i], displaceY + designY[i]);
-			}
-		}
-	}
-	
-	private void drawDiamondIcon(int startX, int startY) {
-		
-		int displaceX = ((ICONSIZE - 12) / 2) + startX;
-		int displaceY = ((ICONSIZE - 18) / 2) + startY;
-		
-		int[] designX = {0, 1, 1, 2, 3, 3, 4, 5, 5, 6,
-				7, 7, 8, 9, 9, 10, 11, 11, 12, 11,
-				11, 10, 9, 9, 8, 7, 7, 6, 5, 5,
-				4, 3, 3, 2, 1, 1};
-		int[] designY = {9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
-				17, 16, 15, 14, 13, 12, 11, 10, 9, 8,
-				7, 6, 5, 4, 3, 2, 1, 0, 1, 2,
-				3, 4, 5, 6, 7, 8};
-		
-		if (selectedIcon == Icon.DIAMOND) {
-			toggleHighlighting(startX, startY, true);
-			
-			for (int i = 0; i < designX.length; i++) {
-				hidePoint(displaceX + designX[i], displaceY + designY[i]);
-			}
-		} else {
-			toggleHighlighting(startX, startY, false);
-			
-			for (int i = 0; i < designX.length; i++) {
-				showPoint(displaceX + designX[i], displaceY + designY[i]);
-			}
-		}
-	}
-	
-	private void drawTriangleIcon(int startX, int startY) {
-		
-		int displaceX = ((ICONSIZE - 14) / 2) + startX;
-		int displaceY = ((ICONSIZE - 11) / 2) + startY;
-		
-		int[] designX = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-				10, 11, 12, 13, 14, 14, 13, 12, 12, 11,
-				10, 10, 9, 8, 8, 7, 6, 6, 5, 4,
-				4, 3, 2, 2, 1, 0};
-		int[] designY = {11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
-				11, 11, 11, 11, 11, 10, 9, 8, 7, 6,
-				5, 4, 3, 2, 1, 0, 1, 2, 3, 4,
-				5, 6, 7, 8, 9, 10};
-		
-		if (selectedIcon == Icon.TRIANGLE) {
-			toggleHighlighting(startX, startY, true);
-			
-			for (int i = 0; i < designX.length; i++) {
-				hidePoint(displaceX + designX[i], displaceY + designY[i]);
-			}
-		} else {
-			toggleHighlighting(startX, startY, false);
-			
-			for (int i = 0; i < designX.length; i++) {
-				showPoint(displaceX + designX[i], displaceY + designY[i]);
-			}
-		}
-	}
-	
-	private void drawArrowIcon(int startX, int startY) {
-		
-		int displaceX = ((ICONSIZE - 8) / 2) + startX;
-		int displaceY = ((ICONSIZE - 13) / 2) + startY;
-		
-		int[] designX = {0, 1, 1, 2, 2, 2, 3, 3, 3, 3,
-				4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-				4, 4, 4, 4, 5, 5, 5, 5, 6, 6,
-				6, 7, 7, 8};
-		int[] designY = {4, 3, 4, 2, 3, 4, 1, 2, 3, 4,
-				0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-				10, 11, 12, 13, 1, 2, 3, 4, 2, 3,
-				4, 3, 4, 4};
-		
-		if (selectedIcon == Icon.ARROW) {
-			toggleHighlighting(startX, startY, true);
-			
-			for (int i = 0; i < designX.length; i++) {
-				hidePoint(displaceX + designX[i], displaceY + designY[i]);
-			}
-		} else {
-			toggleHighlighting(startX, startY, false);
-			
-			for (int i = 0; i < designX.length; i++) {
-				showPoint(displaceX + designX[i], displaceY + designY[i]);
-			}
-		}
-	}
-	
-	private void drawLineIcon(int startX, int startY) {
-		
-		int displaceX = ((ICONSIZE - 8) / 2) + startX;
-		int displaceY = ((ICONSIZE - 16) / 2) + startY;
-		
-		int[] designX = {0, 0, 1, 1, 1, 2, 2, 2, 3, 3,
-				3, 4, 4, 4, 5, 5, 5, 6, 6, 6,
-				7, 7, 7, 8, 8};
-		int[] designY = {16, 15, 15, 14, 13, 13, 12, 11, 11, 10,
-				9, 9, 8, 7, 7, 6, 5, 5, 4, 3,
-				3, 2, 1, 1, 0};
-		
-		if (selectedIcon == Icon.LINE) {
-			toggleHighlighting(startX, startY, true);
-			
-			for (int i = 0; i < designX.length; i++) {
-				hidePoint(displaceX + designX[i], displaceY + designY[i]);
-			}
-		} else {
-			toggleHighlighting(startX, startY, false);
-			
-			for (int i = 0; i < designX.length; i++) {
-				showPoint(displaceX + designX[i], displaceY + designY[i]);
-			}
-		}
-	}
-	
-	private void drawTrashIcon(int startX, int startY) {
-		
-		int displaceX = ((ICONSIZE - 10) / 2) + startX;
-		int displaceY = ((ICONSIZE - 17) / 2) + startY;
-		
-		int[] designX = {0, 0, 0, 1, 1, 1, 1, 1, 1, 1,
-				1, 1, 1, 1, 1, 1, 2, 3, 4, 5,
-				6, 7, 8, 9, 9, 9, 9, 9, 9, 9,
-				9, 9, 9, 9, 9, 9, 8, 7, 6, 5,
-				4, 3, 2, 3, 3, 3, 3, 3, 5, 5,
-				5, 5, 7, 7, 7, 7, 7, 4, 3, 3,
-				3, 4, 5, 6, 7, 7, 7, 6, 10, 10,
-				10, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-				3, 4, 5, 6, 7};
-		int[] designY = {3, 4, 5, 5, 6, 7, 8, 9, 10, 11,
-				12, 13, 14, 15, 16, 17, 17, 17, 17, 17,
-				17, 17, 17, 17, 16, 15, 14, 13, 12, 11,
-				10, 9, 8, 7, 6, 5, 5, 5, 5, 5,
-				5, 5, 5, 11, 12, 13, 14, 15, 12, 13,
-				14, 15, 11, 12, 13, 14, 15, 7, 7, 8,
-				9, 9, 9, 9, 9, 8, 7, 7, 5, 4,
-				3, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-				1, 0, 0, 0, 1};
-		
-		if (selectedIcon == Icon.TRASH) {
-			toggleHighlighting(startX, startY, true);
-			
-			for (int i = 0; i < designX.length; i++) {
-				hidePoint(displaceX + designX[i], displaceY + designY[i]);
-			}
-		} else {
-			toggleHighlighting(startX, startY, false);
-			
-			for (int i = 0; i < designX.length; i++) {
-				showPoint(displaceX + designX[i], displaceY + designY[i]);
-			}
-		}
+		return max;
 	}
 	
 	private void toggleHighlighting(int startX, int startY, boolean isHighlighted) {
